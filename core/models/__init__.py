7b6c277231623517f4ea3165b3223d5f6e889c93
Михаил Скорняков <misha-skor2012@yandex.ru>
2023-10-10 20:56:15 +0300

    create posts model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "DatabaseHelper",
     "Product",
     "User",
+    "Post",
 )
 
 from .base import Base
 from .product import Product
 from .db_helper import db_helper, DatabaseHelper
 from .user import User
+from .post import Post
---
+++ core/models/post.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, ForeignKey
+
+from .base import Base
+
+
+class Post(Base):
+    title: Mapped[str] = mapped_column(String(100))
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+    price: Mapped[int]
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        # nullable=False,
+    )

4d0bcb5a7cde8ba6979f4ba66f7898c8907cc538
Михаил Скорняков <misha-skor2012@yandex.ru>
2023-10-10 12:27:05 +0300

    add gitignore, install pydantic with email validator, API Router, CRUD, Annotated validation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, Body
-from pydantic import EmailStr
+from typing import Annotated
+
+from fastapi import FastAPI, Body, Path
+
+
 import uvicorn
+from users.views import router as users_router
+from items_views import router as items_router
 
 app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
 
 @app.get("/")
@@ -12,42 +19,10 @@ def hello_index():
     }
 
 
-@app.get("/items/")
-def list_items():
-    return [
-        "Item1",
-        "Item2",
-        "Item3",
-        "Item3",
-    ]
-
-
-@app.get("/items/latest/")
-def get_latest_item():
-    return {
-        "item": {"id": "0", "name": "latest"},
-    }
-
-
 @app.get("/hello/")
 def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"Hello {name}"}
-
-
-@app.post("/users/")
-def create_user(email: EmailStr = Body()):
-    return {
-        "message": "success",
-        "email": email,
-    }
-
-
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: int):
-    return {
-        "item": {"id": item_id},
-    }
 
 
 if __name__ == "__main__":

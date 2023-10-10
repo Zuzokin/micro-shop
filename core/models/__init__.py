b036a30ad37786cc74ac940a215a405b70a0848e
Михаил Скорняков <misha-skor2012@yandex.ru>
2023-10-10 20:42:23 +0300

    create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,8 +3,10 @@ __all__ = (
     "db_helper",
     "DatabaseHelper",
     "Product",
+    "User",
 )
 
 from .base import Base
 from .product import Product
 from .db_helper import db_helper, DatabaseHelper
+from .user import User
